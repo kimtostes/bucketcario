a28c6a6d4200ce948cc5b37345ae475c73bd8f26
kimtostes <kimtostes@gmail.com>
2021-03-01 18:08:58 -0300

    Update vendas_marca_linha.py

--- 2-transform/vendas_marca_linha/vendas_marca_linha.py
+++ 2-transform/vendas_marca_linha/vendas_marca_linha.py
@@ -16,6 +16,8 @@ spark.conf.set('temporaryGcsBucket', 'bucketcario1')
 
 vendas_marca_linha = spark.read.format("bigquery").option('table', 'grupo-boticario-305900.raw_grupo_boticario.vendas').load()
 
+vendas_marca_linha = vendas_marca_linha.dropDuplicates()
+
 vendas_marca_linha = vendas_marca_linha.select(F.col("MARCA"), F.col("ID_LINHA").cast("long"), F.col("LINHA"), F.col("QTD_VENDA").cast("long"))
 vendas_marca_linha = vendas_marca_linha.groupBy("MARCA", "ID_LINHA", "LINHA").sum("QTD_VENDA").alias("QTD_VENDA").sort(F.col("LINHA").asc())
 vendas_marca_linha = vendas_marca_linha.select(F.col("MARCA"), F.col("ID_LINHA"), F.col("LINHA"), F.col("sum(QTD_VENDA)").alias("QTD_VENDA"))
